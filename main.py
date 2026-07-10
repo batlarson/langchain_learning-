fea3d2d14d5b810099b6df01c28d15314917be7f
Ibon Mugica <iboedu@hotmail.com>
2026-07-10 02:23:20 +0200

    Añadir memoria a modelo LLM Gemini para poder mantener conversaciones

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
 
 load_dotenv()
 
@@ -9,12 +10,22 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise RuntimeError("Falta GEMINI_API_KEY en el archivo .env")
 
-model = ChatGoogleGenerativeAI(model="gemini-3.5-flash", api_key=api_key)
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash", 
+    api_key=api_key,
+    request_timeout=30
+)
 
-messages = [
-    ("system", "Eres un asistente útil que responde en español."),
-    ("human", "Dime un dato curioso sobre Python en una frase.")
-]
+historial = []
 
-ai_msg = model.invoke(messages)
-print(ai_msg.content[0]['text'])
+
+while True:
+    pregunta = input("Tú: ")
+    if pregunta.lower() == "salir":
+        break
+    
+    historial.append(HumanMessage(content=pregunta))
+    respuesta = model.invoke(historial)
+    historial.append(AIMessage(content=respuesta.content))
+    
+    print(f"IA: {respuesta.content[0]['text']}\n")
